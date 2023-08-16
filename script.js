27860d8332d362f43752247f78c643f07a9ae1f8
AnshPatel08 <anshhpatelril@gmail.com>
2023-08-16 12:12:01 +0530

    completed

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-console.log("Welcome to Tic-Tac-Toe")
-
+console.log("Welcome to Tic Tac Toe")
 let music = new Audio("music.mp3")
 let audioTurn = new Audio("ting.mp3")
 let gameover = new Audio("gameover.mp3")
-let isgameover = false;
- 
-// finction to change turn
 let turn = "X"
-const changeTurn = ()=> {
-    return turn === "X"?"0": "X"
+let isgameover = false;
+
+// Function to change the turn
+const changeTurn = ()=>{
+    return turn === "X"? "0": "X"
 }
 
-// function to check for the win
-const checkWin = ()=>{.innerText + "Won"
-let boxtext = document.getElementsByClassName('boxtext');
-    let wins = [ 
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6],
+// Function to check for a win
+const checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
     ]
     wins.forEach(e =>{
-        if((boxtext[e[0]].innerText ===boxtext[e[1]].innerText) && (boxtext[e[0]].innerText ===boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")){
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText + "Won"
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
             isgameover = true
-            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px"
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            document.querySelector(".line").style.width = "20vw";
         }
-
-
     })
-
 }
-// game logic
+
+// Game Logic
 // music.play()
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element =>{
@@ -46,14 +45,23 @@ Array.from(boxes).forEach(element =>{
             turn = changeTurn();
             audioTurn.play();
             checkWin();
-            if(!isgameover){
-                document.getElementsByClassName("info")[0].innerText = "Turn for" + turn;
-                
-            }
+            if (!isgameover){
+                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+            } 
         }
     })
-
 })
 
-
+// Add onclick listener to reset button
+reset.addEventListener('click', ()=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X"; 
+    isgameover = false
+    document.querySelector(".line").style.width = "0vw";
+    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
+})
 
